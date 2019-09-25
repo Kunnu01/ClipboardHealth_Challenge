9df2c0b9639699715c91bff4cd85bc451f460ed7
kunnu01 <kunalbhatia2409@gmail.com>
2019-09-26 00:15:19 +0530

    setup project

---
+++ data/index.js
@@ -0,0 +1,58 @@
+const ShiftList = [
+  {
+    _id: 1,
+    worker: 'Worker 1',
+    time: 9, // Total hrs worked for a particular shift
+    rate: 20, // Worker rate per hour
+  },
+  {
+    _id: 2,
+    worker: 'Worker 2',
+    time: 7, // Total hrs worked for a particular shift
+    rate: 25, // Worker rate per hour
+  },
+  {
+    _id: 3,
+    worker: 'Worker 3',
+    time: 8, // Total hrs worked for a particular shift
+    rate: 23, // Worker rate per hour
+  },
+  {
+    _id: 4,
+    worker: 'Worker 4',
+    time: 8, // Total hrs worked for a particular shift
+    rate: 27, // Worker rate per hour
+  },
+  {
+    _id: 5,
+    worker: 'Worker 5',
+    time: 11, // Total hrs worked for a particular shift
+    rate: 23, // Worker rate per hour
+  },
+  {
+    _id: 6,
+    worker: 'Worker 6',
+    time: 6, // Total hrs worked for a particular shift
+    rate: 22, // Worker rate per hour
+  },
+  {
+    _id: 7,
+    worker: 'Worker 7',
+    time: 8, // Total hrs worked for a particular shift
+    rate: 20, // Worker rate per hour
+  },
+  {
+    _id: 8,
+    worker: 'Worker 8',
+    time: 10, // Total hrs worked for a particular shift
+    rate: 21, // Worker rate per hour
+  },
+  {
+    _id: 9,
+    worker: 'Worker 9',
+    time: 8, // Total hrs worked for a particular shift
+    rate: 24, // Worker rate per hour
+  },
+]
+
+module.exports = { ShiftList }
