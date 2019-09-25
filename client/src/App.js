81b0d48309576a3223530ddd0b8d6929fd64645c
kunnu01 <kunalbhatia2409@gmail.com>
2019-09-26 00:25:54 +0530

    add react routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import { Route, Switch, Link, BrowserRouter } from 'react-router-dom';
 import logo from './bunny.png';
 import './App.css';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import { theme } from './theme';
+import { Button } from '@material-ui/core';
+
+function Home() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Welcome to Clipboard Health
+      </p>
+      <Button variant="outlined" color="secondary" component={Link} to="/shifts">
+        Click here
+      </Button>
+    </header>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <MuiThemeProvider theme={theme}>  
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Welcome to Clipboard Health
-          </p>
-        </header>
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter>        
+          <Switch>
+            <Route path="/shifts" component={() => <h1>Shift Details</h1>} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </BrowserRouter>
       </MuiThemeProvider>
     </div>
   );
